2e05e2062e34cb3c0b4c37f7630a0bed66c228b2
Joao Pedro <ppvidalsantos@gmail.com>
2024-12-26 23:56:00 -0300

    configurando as rotas

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,7 +1,15 @@
 const express = require('express');
 const router = express.Router();
 const PessoaController = require('../controllers/PessoaController');
+const ProdutoController = require('../controllers/ProdutoController');
+const CnpjController = require('../controllers/CnpjController');
 
+//Rotas de INSERT
 router.post('/novaPessoa', PessoaController.novaPessoa);
+router.post('/novoProduto', ProdutoController.novoProduto);
+
+//Rotas de SELECT
+router.get('/consultaCnpj/:cnpj', CnpjController.consultaCnpj);
+
 
 module.exports = router;
