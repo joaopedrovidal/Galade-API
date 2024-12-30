ecb181f5089acebd8255e7c38b7b0f3b36c4f075
joaopedrovidal <ppvidalsantos@gmail.com>
2024-12-30 14:47:29 -0300

    Adicionandoi nonas rotas na aplicação

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -3,10 +3,15 @@ const router = express.Router();
 const PessoaController = require('../controllers/PessoaController');
 const ProdutoController = require('../controllers/ProdutoController');
 const CnpjController = require('../controllers/CnpjController');
+const EmpresaController = require('../controllers/EmpresaController');
+const LoginController = require('../controllers/LoginController');
 
 //Rotas de INSERT
 router.post('/novaPessoa', PessoaController.novaPessoa);
 router.post('/novoProduto', ProdutoController.novoProduto);
+router.post('/novaEmpresa', EmpresaController.novaEmpresa);
+router.post('/login', LoginController.login);
+router.post('/cadastro', LoginController.cadastro);
 
 //Rotas de SELECT
 router.get('/consultaCnpj/:cnpj', CnpjController.consultaCnpj);
